acc51f0fa81476645bebdf65e7d8349b39b6704b
Cas Bex <cas.bex@kuleuven.be>
2025-06-11 14:50:36 +0200

    Add trigger

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,15 +10,21 @@ using Aqua
 end
 
 const ctx = Context()
-const addr = "inproc://logger"
+const bind_addr = "inproc://testlogger"
+const connect_addr = "inproc://testlogger"
+const trigger_addr = "inproc://trigger"
 const timeout_sec = 30
 
 # receiver is asynchronous to prevent tests from hanging
-function spawn_receiver(ctx, addr, msg_list, c)
+function spawn_receiver(ctx, msg_list, c)
     return @spawn begin
         receiver = Socket(ctx, SUB)
         subscribe(receiver, "")
-        connect(receiver, addr)
+        connect(receiver, connect_addr)
+        trigger = Socket(ctx, REQ)
+        connect(trigger, trigger_addr)
+        send(trigger, "Ready")
+        recv(trigger)
         for msg in msg_list
             r = recv(receiver, String)
             println("Got: " * r)
@@ -42,11 +48,15 @@ end
 
 @testset "Main" begin
     c = Channel{String}(32)
-    test_task = spawn_receiver(ctx, addr, ["Error", "Warning", "Info", "Debug"], c)
+    test_task = spawn_receiver(ctx, ["Error", "Warning", "Info", "Debug"], c)
 
     logsock = Socket(ctx, PUB)
-    bind(logsock, addr)
+    bind(logsock, bind_addr)
     logger = ZMQLogger(logsock, Logging.BelowMinLevel)
+    trigger = Socket(ctx, REP)
+    bind(trigger, trigger_addr)
+    recv(trigger)
+    send(trigger, "Ok")
 
     Logging.with_logger(logger) do
         Logging.@error "Error"
@@ -59,8 +69,10 @@ end
     @test take!(c) == "Ok"
 
     c = Channel{String}(32)
-    test_task = spawn_receiver(ctx, addr, ["Error", "Warning"], c)
+    test_task = spawn_receiver(ctx, ["Error", "Warning"], c)
     logger = ZMQLogger(logsock, Logging.Warn)
+    recv(trigger)
+    send(trigger, "Ok")
 
     Logging.with_logger(logger) do
         Logging.@error "Error"
