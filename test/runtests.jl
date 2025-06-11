1f6aee10e0fd496b104fc1a825f8cb52bf3dee91
CasBex <123587431+CasBex@users.noreply.github.com>
2025-06-11 14:57:17 +0200

    Fix CI and remove code coverage (#2)
    * Remove requirement for codecov token
    
    * More generous timeout
    
    * Debug
    
    * Increase channel capacity
    
    * Add trigger
    
    * Remove debug and add explanation

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,53 +9,83 @@ using Aqua
     Aqua.test_all(LoggingZMQ)
 end
 
-ctx = Context()
-addr = "inproc://logger"
+const ctx = Context()
+const log_addr = "inproc://testlogger"
+const trigger_addr = "inproc://trigger"
+const timeout_sec = 30
 
 # receiver is asynchronous to prevent tests from hanging
-function spawn_receiver(ctx, addr, msg_list)
+function spawn_receiver(ctx, msg_list, c)
     return @spawn begin
         receiver = Socket(ctx, SUB)
         subscribe(receiver, "")
-        connect(receiver, addr)
+        connect(receiver, log_addr)
+        # Indicate to the main testing code that task is ready to receive logs
+        # Without this, CI doesn't run for some reason
+        trigger = Socket(ctx, REQ)
+        connect(trigger, trigger_addr)
+        send(trigger, "Ready")
+        recv(trigger)
         for msg in msg_list
             r = recv(receiver, String)
             # println("Got: " * r)
             # println("Expected: " * msg)
             if r != msg
                 close(receiver)
-                return false
+                put!(c, "Failed")
+                return
             end
         end
         close(receiver)
-        return true
+        put!(c, "Ok")
+        return
     end
 end
 
+# cause tests to fail when timeout
+function timeout(s, c)
+    sleep(s)
+    return put!(c, "Timeout")
+end
+
 @testset "Main" begin
-    test_task = spawn_receiver(ctx, addr, ["Error", "Warning", "Info", "Debug"])
+    c = Channel{String}(32)
+    test_task = spawn_receiver(ctx, ["Error", "Warning", "Info", "Debug"], c)
+
     logsock = Socket(ctx, PUB)
-    bind(logsock, addr)
+    bind(logsock, log_addr)
     logger = ZMQLogger(logsock, Logging.BelowMinLevel)
 
+    # Wait until receiver indicates ok to start
+    trigger = Socket(ctx, REP)
+    bind(trigger, trigger_addr)
+    recv(trigger)
+    send(trigger, "Ok")
+
     Logging.with_logger(logger) do
         Logging.@error "Error"
         Logging.@warn "Warning"
         Logging.@info "Info"
         Logging.@debug "Debug"
     end
-    sleep(1)
-    @test istaskdone(test_task) && fetch(test_task)
 
-    test_task = spawn_receiver(ctx, addr, ["Error", "Warning"])
+    @spawn timeout(timeout_sec, c)
+    @test take!(c) == "Ok"
+
+    c = Channel{String}(32)
+    test_task = spawn_receiver(ctx, ["Error", "Warning"], c)
     logger = ZMQLogger(logsock, Logging.Warn)
 
+    # Wait until receiver indicates ok to start
+    recv(trigger)
+    send(trigger, "Ok")
+
     Logging.with_logger(logger) do
         Logging.@error "Error"
         Logging.@warn "Warning"
         Logging.@info "Info"
         Logging.@debug "Debug"
     end
-    sleep(1)
-    @test istaskdone(test_task) && fetch(test_task)
+    @spawn timeout(timeout_sec, c)
+    @test take!(c) == "Ok"
 end
